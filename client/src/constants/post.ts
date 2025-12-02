f035c5b36450e493cdf904636d2c7d3dd4ef0ea4
NancyLuckyTo <Cxzl5920++>
2025-12-02 18:49:39 +0800

    refactor(client): 加大首页瀑布流触发加载的感应区域

--- client/src/constants/post.ts
+++ client/src/constants/post.ts
@@ -1,4 +1,4 @@
-export const ROOT_MARGIN_VALUE = '0px 0px 500px 0px' // 瀑布流布居中触发加载的距离
+export const ROOT_MARGIN_VALUE = '0px 0px 800px 0px' // 瀑布流布居中触发加载的距离
 
 export const BODY_MAX_LENGTH = 5000 // 笔记正文最大长度
 export const BODY_PREVIEW_MAX_LENGTH = 50 // 预览正文最大长度
