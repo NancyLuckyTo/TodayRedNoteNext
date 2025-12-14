52bf925829e4bc339a58fdf4888ee057d452fd14
NancyLuckyTo <Cxzl5920++>
2025-12-14 16:06:02 +0800

    refactor(all): 迁移 next-app 至根目录, 删除 client / server

--- client/src/components/ui/toast/index.ts
+++
@@ -1,9 +0,0 @@
-export { Toast } from './Toast'
-export { ToastProvider } from './ToastProvider'
-export type { ToastPosition } from './ToastProvider'
-export { toast, useToastStore } from '@/stores/toastStore'
-export type {
-  Toast as ToastType,
-  ToastOptions,
-  ToastType as ToastVariant,
-} from '@/stores/toastStore'
--- next-app/src/constants/post.ts
+++
@@ -1,7 +0,0 @@
-export const ROOT_MARGIN_VALUE = '0px 0px 1200px 0px' // 瀑布流布居中触发加载的距离
-
-export const BODY_MAX_LENGTH = 5000 // 笔记正文最大长度
-export const BODY_PREVIEW_MAX_LENGTH = 50 // 预览正文最大长度
-
-export const IMAGE_MAX_COUNT = 18 // 最多上传图片数量
-export const IMAGE_MAX_WIDTH = 800 // 图片最大宽度
